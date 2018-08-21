808975000bacae207d6d0cc21efaba216dab8577
beautifulcodestar <apple930108@gmail.com>
2018-08-21 22:30:40 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'get-request-make-table';
+  data: any;
+  error: string;
+  
+  constructor(private http: HttpClient) {
+    this.makeCall();
+  }
+  
+  makeCall(): void {
+    const url = "http://dev.api.fooddocs.ee/testtask";
+
+    this.http.get(url).subscribe((res: HttpResponse<object>) => {
+      console.log(res);
+      this.data = res;
+    }, (err: HttpErrorResponse) => {
+      console.log(err);
+
+    })
+  }
+
+  hideRow(id: number): void {
+    this.data.splice(id, 1);
+  }
+
 }
